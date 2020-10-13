929b4d08a07c9ca07c4c06b4532ace8289377eb0
fanfei-mbp <1610184295@qq.com>
2020-10-13 17:16:38 +0800

    工具/windows安装mingw

--- go-test/slice-range/main.go
+++ go-test/slice-range/main.go
@@ -1,13 +1,13 @@
 package main
 
-import "log"
+import "fmt"
 
 type issue struct {
 	A int
 }
 
 func main()  {
-	log.Println(float64(1.0/2.0))
+	//log.Println(float64(1.0/2.0))
 	//arr := []issue{
 	//	{A:1},
 	//	{A:2},
@@ -19,4 +19,11 @@ func main()  {
 	//	arr2 = append(arr2, c)
 	//}
 	//log.Println(arr2)
+
+	m := make(map[int]int)
+	s := []int{1,2,3}
+	for k, v := range s {
+		m[k] = v
+	}
+	fmt.Print(m)
 }
